eee7944ed432a57c2e8491bbf5d1102619643c60
“hadie <2029291740@qq.com>
2019-09-15 21:30:57 +0800

    change

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 import os 
 from dotenv import load_dotenv
 
-dotenv_path = os.path.join(os.path.dirname(__file__),'env')
+dotenv_path = os.path.join(os.path.dirname(__file__),'.env')
 if os.path.exists(dotenv_path):
 	load_dotenv(dotenv_path)
 	
-from movielist import app
+from movielist import app 
